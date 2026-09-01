ed059d0de3bebc8d351696d3b6e2a0c7ba67073e
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-01 19:54:03 +0530

    Time: 7 ms (69.48%), Space: 22.9 MB (58.49%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,39 +1,78 @@
+// class Solution {
+// public:
+
+//     int minEatingSpeed(vector<int>& piles, int h) {
+
+//         long long left = 1;
+//         long long right = 1000000000;
+
+//         while (left <= right) {
+
+//             long long mid = left + (right - left) / 2;
+
+//             if (canEatInTime(piles, mid, h)) {
+//                 right = mid - 1;
+//             }
+//             else {
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return (int)left;
+//     }
+
+//     bool canEatInTime(vector<int>& piles, long long k, int h) {
+
+//         long long hours = 0;
+
+//         for (int pile : piles) {
+
+//             hours += pile / k;
+
+//             if (pile % k != 0) {
+//                 hours++;
+//             }
+//         }
+
+//         return hours <= h;
+//     }
+// };
+
 class Solution {
 public:
-
-    int minEatingSpeed(vector<int>& piles, int h) {
-
-        long long left = 1;
-        long long right = 1000000000;
-
-        while (left <= right) {
-
-            long long mid = left + (right - left) / 2;
-
-            if (canEatInTime(piles, mid, h)) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
+    
+        long long calculateTotalHours(vector<int>& piles, int speed) {
+        long long totalH = 0;
+        for (int bananas : piles) {
+            totalH += ((long long)bananas +speed -1) / speed;
         }
-
-        return (int)left;
+        return totalH;
     }
 
-    bool canEatInTime(vector<int>& piles, long long k, int h) {
+   
+    int minEatingSpeed(vector<int>& piles, int h) {
+     
+        int maxPile = *max_element(piles.begin(), piles.end());
 
-        long long hours = 0;
+       
+        int low = 1, high = maxPile;
+        int ans = maxPile;
 
-        for (int pile : piles) {
+      
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long long totalH = calculateTotalHours(piles, mid);
 
-            hours += pile / k;
-
-            if (pile % k != 0) {
-                hours++;
+           
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+        
+            else {
+                low = mid + 1;
             }
         }
-
-        return hours <= h;
+        return ans;
     }
 };
