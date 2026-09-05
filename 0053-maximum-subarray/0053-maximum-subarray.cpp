8eb386ac4b89addb34aac03c6703932b0d5a536d
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-05 22:32:03 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr = nums[0];
+        int maxS = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            curr = max(nums[i],curr+nums[i]);
+            maxS = max(maxS, curr);
+        }
+
+        return maxS;
+        
+    }
+};
