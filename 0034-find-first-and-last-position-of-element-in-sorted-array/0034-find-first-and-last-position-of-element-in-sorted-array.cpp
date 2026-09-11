e7cfdcb1170c8102c7a3aad5e09c7fec5b7aff8a
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-11 20:37:37 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,67 @@
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int> res = {-1, -1};
+//         int left = BinaryS(nums, target, true);
+//         int right = BinaryS(nums, target, false);
+//         res[0] = left;
+//         res[1] = right;
+//         return res;
+//     }
+
+//     int BinaryS(vector<int>& nums, int target, bool isSearch){
+//         int left = 0;
+//         int right = nums.size() - 1;
+//         int idx = -1;
+
+//         while(left <= right){
+//             int mid = left + (right - left)/2;
+
+//             if(nums[mid] > target){
+//                 right = mid - 1;
+//             } else if(nums[mid] < target){
+//                 left = mid + 1;
+
+//             } else { 
+//                 idx = mid;
+//                 if(isSearch){
+//                     right = mid - 1;
+//                 } else {
+//                     left = mid + 1;
+//                 }
+//             }
+//         }
+
+//         return idx;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res = {-1, -1};
-        int left = BinaryS(nums, target, true);
-        int right = BinaryS(nums, target, false);
-        res[0] = left;
-        res[1] = right;
-        return res;
+        return {findBound(nums, target, true), findBound(nums, target, false)};
     }
 
-    int BinaryS(vector<int>& nums, int target, bool isSearch){
-        int left = 0;
-        int right = nums.size() - 1;
-        int idx = -1;
+private:
+    int findBound(vector<int>& nums, int target, bool searchLeft) {
+        int left = 0, right = nums.size() - 1, idx = -1;
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            if(nums[mid] > target){
-                right = mid - 1;
-            } else if(nums[mid] < target){
-                left = mid + 1;
-
-            } else { 
+            if (nums[mid] == target) {
                 idx = mid;
-                if(isSearch){
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+                if (searchLeft) right = mid - 1;
+                else left = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
-
         return idx;
     }
 };
