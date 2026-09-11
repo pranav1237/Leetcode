2fca19d817893c005ce5adebf722628bf55a72ed
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-11 19:43:20 +0530

    Time: 0 ms (100%), Space: 17.9 MB (80.81%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,73 @@
+// class Solution {
+// public:
+//     bool search(vector<int>& nums, int target) {
+//         int left = 0;
+//         int right = nums.size() - 1;
+
+//         while(left <= right){
+//             int mid = (left + right) / 2;
+
+//             if(nums[mid] == target){
+//                 return true;
+//             }
+
+//             if(nums[mid] == nums[left]){
+//                 left++;
+//                 continue;
+//             }
+
+//             if(nums[left] <= nums[mid]){
+//                 if(nums[left] <= target && target < nums[mid]){
+//                     right = mid - 1;
+//                 } else{
+//                     left = mid + 1;
+//                 }
+//             }else {
+//                 if(nums[mid] < target && target <= nums[right]){
+//                     left = mid + 1;
+//                 }else{
+//                     right = mid-1;
+//                 }
+//             }
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+        int left = 0, right = nums.size() - 1;
 
-        while(left <= right){
+        while (left <= right) {
             int mid = (left + right) / 2;
 
-            if(nums[mid] == target){
-                return true;
-            }
+            if (nums[mid] == target) return true;
 
-            if(nums[mid] == nums[left]){
+            // Skip duplicates
+            if (nums[left] == nums[mid]) {
                 left++;
                 continue;
             }
 
-            if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target < nums[mid]){
+            // Left half is sorted
+            if (nums[left] < nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
-                } else{
+                } else {
                     left = mid + 1;
                 }
-            }else {
-                if(nums[mid] < target && target <= nums[right]){
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
-                }else{
-                    right = mid-1;
+                } else {
+                    right = mid - 1;
                 }
             }
         }
-
         return false;
     }
 };
