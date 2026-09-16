9f9f65195549c3aa35a3306f2b2382e934fc0227
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-16 20:32:09 +0530

    Time: 4 ms (67.31%), Space: 134.3 MB (84.69%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,25 +1,56 @@
+// class Solution {
+// public:
+//     using ll = long long;
+//     vector<int> getAverages(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> ans(n, -1);
+//         if (k == 0) return nums; // no averaging needed
+//         if (2 * k + 1 > n) return ans; // window too large
+
+//         ll windowSum = 0;
+//         int windowSize = 2 * k + 1;
+
+//         // initial window
+//         for (int i = 0; i < windowSize; i++) {
+//             windowSum += nums[i];
+//         }
+//         ans[k] = windowSum / windowSize;
+
+//         // slide the window
+//         for (int i = windowSize; i < n; i++) {
+//             windowSum += nums[i] - nums[i - windowSize];
+//             ans[i - k] = windowSum / windowSize;
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    using ll = long long;
     vector<int> getAverages(vector<int>& nums, int k) {
         int n = nums.size();
+        int x = 2 * k + 1;
+
         vector<int> ans(n, -1);
-        if (k == 0) return nums; // no averaging needed
-        if (2 * k + 1 > n) return ans; // window too large
 
-        ll windowSum = 0;
-        int windowSize = 2 * k + 1;
+        if (n < x)
+            return ans;
 
-        // initial window
-        for (int i = 0; i < windowSize; i++) {
-            windowSum += nums[i];
-        }
-        ans[k] = windowSum / windowSize;
+        long long sum = 0;
 
-        // slide the window
-        for (int i = windowSize; i < n; i++) {
-            windowSum += nums[i] - nums[i - windowSize];
-            ans[i - k] = windowSum / windowSize;
+        for (int i = 0; i < x; i++)
+            sum += nums[i];
+
+        for (int i = k; i < n - k; i++) {
+
+            ans[i] = sum / x;
+
+            if (i + 1 < n - k) {
+                sum -= nums[i - k];
+                sum += nums[i + k + 1];
+            }
         }
 
         return ans;
