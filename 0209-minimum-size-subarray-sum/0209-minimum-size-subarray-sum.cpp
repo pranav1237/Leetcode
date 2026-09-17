3494a9b6ae663ff599ceba5f0f31f910d2c9e446
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-17 22:32:13 +0530

    Time: 0 ms (100%), Space: 42 MB (41.31%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,23 +1,43 @@
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int minLen = numeric_limits<int>::max();
+//         int left = 0;
+//         int currSum = 0;
+
+//         for(int right = 0; right < nums.size(); right++){
+//             currSum += nums[right];
+
+//             while(currSum >= target){
+//                 if (right - left + 1 < minLen){
+//                     minLen = right - left + 1;
+//                 }
+
+//                 currSum -= nums[left];
+//                 left++;
+//             }
+//         }
+
+//         return minLen != numeric_limits<int>::max() ? minLen : 0;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int minLen = numeric_limits<int>::max();
-        int left = 0;
-        int currSum = 0;
+        int left = 0, sum = 0, minLen = INT_MAX;
 
-        for(int right = 0; right < nums.size(); right++){
-            currSum += nums[right];
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
 
-            while(currSum >= target){
-                if (right - left + 1 < minLen){
-                    minLen = right - left + 1;
-                }
-
-                currSum -= nums[left];
-                left++;
+            while (sum >= target) {
+                minLen = min(minLen, right - left + 1);
+                sum -= nums[left++];
             }
         }
 
-        return minLen != numeric_limits<int>::max() ? minLen : 0;
+        return minLen == INT_MAX ? 0 : minLen;
     }
 };
