4a2fe3345bf62c7cc0c724368cda86c092926147
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-09-19 21:00:46 +0530

    Time: 4 ms (96.16%), Space: 79.6 MB (99.01%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -22,22 +22,51 @@
 // };
 
 
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+//         unordered_map<int, int> basket;
+//         int left = 0, maxLen = 0;
+
+//         for (int right = 0; right < fruits.size(); right++) {
+//             basket[fruits[right]]++;
+
+//             while (basket.size() > 2) {
+//                 if (--basket[fruits[left]] == 0) basket.erase(fruits[left]);
+//                 left++;
+//             }
+
+//             maxLen = max(maxLen, right - left + 1);
+//         }
+//         return maxLen;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> basket;
-        int left = 0, maxLen = 0;
-
-        for (int right = 0; right < fruits.size(); right++) {
-            basket[fruits[right]]++;
-
-            while (basket.size() > 2) {
-                if (--basket[fruits[left]] == 0) basket.erase(fruits[left]);
-                left++;
+        if (fruits.size() < 2) return fruits.size();
+        int l = 0, r = 0;
+        while (r < fruits.size() && fruits[r] == fruits[l]) r++;
+        if (r == fruits.size()) return fruits.size();
+        int basket1 = fruits[l], basket2 = fruits[r], maxSize = (r - l) + 1;
+        for (r++; r < fruits.size(); r++) {
+            if (fruits[r] == basket1 || fruits[r] == basket2) {
+                maxSize = max(maxSize, (r - l) + 1);
             }
-
-            maxLen = max(maxLen, right - left + 1);
+            else {
+                for (int i = r - 1; i >= l; i--) {
+                    if (fruits[i] != fruits[r - 1]) {
+                        l = i + 1;
+                        basket1 = fruits[l];
+                        basket2 = fruits[r];
+                        break;
+                    }
+                }
+            }
         }
-        return maxLen;
+        return maxSize;
     }
 };
